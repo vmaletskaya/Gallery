dc701b4fced17679c3ef23ecc19124bf1a75f86c
Vika Maletskaya <v.maletskayaah@gmail.com>
2023-03-03 14:24:58 +0300

    end hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,51 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// const galleryContainer = document.querySelector('.gallery');
 
-// const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-// galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-
-// function createGalleryItemsMarkup(items) {
-//   return items
-//     .map(({ preview, original, description }) => {
-//       return `
-//         <div class="gallery__item">
-//           <a class="gallery__link" href="${original}">
-//             <img
-//               class="gallery__image"
-//               src="${preview}"
-//               data-source="${original}"
-//               alt="${description}"
-//             />
-//           </a>
-//         </div>
-//       `;
-//     })
-//     .join('');
-// }
-
-// galleryContainer.addEventListener('click', onGalleryItemClick);
-
-// function onGalleryItemClick(event) {
-//   event.preventDefault();
-
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-
-//   const largeImageUrl = event.target.dataset.source;
-
-//   openModal(largeImageUrl);
-// }
-
-// const gallery = document.querySelector('.gallery');
-// const modal = basicLightbox.create(document.querySelector('#modal'));
-
-
-
-// console.log(galleryItems);
-
-// Change code below this line
 const galleryContainer = document.querySelector(".gallery");
 
 const galleryImagesMarkup = galleryItems
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryImagesMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}", alt="${description}"/></a>`
+  )
+  .join(" ");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryImagesMarkup);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
